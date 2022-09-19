e5a870ff9217dc06ca4bd25654747592a003508f
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-09-19 16:06:18 +0200

    Change PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const https = require(`https`);
 const bodyParser = require(`body-parser`);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 app.set(`views`, `./views`);
 app.set(`view engine`, `pug`);
